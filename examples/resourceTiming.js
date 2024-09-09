7429c8553f165ecc614cbc9cc2c5c337b565fb5f
csmig <carlsmigielski@gmail.com>
2024-09-09 16:31:52 -0400

    getResourceTimings()

--- examples/resourceTiming.js
+++ examples/resourceTiming.js
@@ -1,10 +1,42 @@
 import {auth, resourceTiming} from '../index.js'
 
+// common for all examples
 const apiBase = 'http://localhost:64001/api'
-const username = 'stigmanadmin'
+
+
+// A. Get timings for one request with pre-fetched token 
 const url = `${apiBase}/collections/30?projection=labels`
+const token = await auth.getAccessToken({username: 'stigmanadmin'})
+const t1 = await resourceTiming.getResourceTiming({url, token})
+console.log(`A: ${t1.name}: ${t1.responseStart - t1.requestStart}`)
+console.log()
 
-const token = await auth.getAccessToken({username})
+// B. Get timings for one request with username provided (token fetched dynamically)
+const t2 = await resourceTiming.getResourceTiming({url, username: 'stigmanadmin'})
+console.log(`B: ${t2.name}: ${t2.responseStart - t2.requestStart}`)
+console.log()
 
-const timing = await resourceTiming.getResourceTiming({url, token})
-console.log(`${timing.name}: ${timing.responseStart - timing.requestStart}`)
+// C. Get timings for multiple requests, some with usernames and one with pre-fetched token
+const requests = [
+  {
+    url: `${apiBase}/collections/30?projection=labels`,
+    token
+  },
+  {
+    url: `${apiBase}/collections/30?projection=labels`,
+    username: 'admin'
+  },
+  {
+    url: `${apiBase}/assets/30?projection=stigs`,
+    username: 'user01'
+  },
+  {
+    url: `${apiBase}/assets/30?projection=stigs`,
+    username: 'user02'
+  }
+]
+
+const timings = await resourceTiming.getResourceTimings(requests)
+for (const time of timings) {
+  console.log(`C: ${time.name}: ${time.responseStart - time.requestStart}`)
+}
