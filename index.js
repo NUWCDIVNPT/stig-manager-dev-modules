c090a34435fb0fc8d1bf71d4880fb7df3fd3a0b3
csmig <carlsmigielski@gmail.com>
2026-04-29 16:30:24 -0400

    import MockOidc correctly

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import * as auth from './lib/auth.js'
 import * as resourceTiming from './lib/resourceTiming.js'
 import {OpenApiOps} from './lib/openApiOps.js'
-import {MockOidc} from './lib/mockOidc.js'
+import MockOidc from './lib/mockOidc.js'
 
 export {
   auth,
